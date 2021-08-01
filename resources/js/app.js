dfbbe8983eeb372413fd92d82aa821561b4a8754
Lawr <lawrboni@gmail.com>
2021-08-02 00:48:25 +0800

    [FE] Create Navbar

--- resources/js/app.js
+++ resources/js/app.js
@@ -11,8 +11,19 @@ import App from './App.vue';
 import VueRouter from 'vue-router';
 import router from './router';
 
-window.Vue = require('vue').default;
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+
+// Import Bootstrap an BootstrapVue CSS files (order is important)
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+// Make BootstrapVue available throughout your project
+Vue.use(BootstrapVue)
+// Optionally install the BootstrapVue icon components plugin
+Vue.use(IconsPlugin)
 Vue.use(VueRouter)
+
+window.Vue = require('vue').default;
 
 function loggedIn() {
   return localStorage.getItem('userToken')
