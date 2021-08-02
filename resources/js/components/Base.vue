e16b63d67a486f6c104334f23b58c6d2ca7cc613
Lawr <lawrboni@gmail.com>
2021-08-02 23:35:57 +0800

    [FE] Display List of Pokemon

---
+++ resources/js/components/Base.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <Navbar />
+    <div class="container">
+      <!-- Display Pokemons -->
+      <div v-if="currentRoute === 'Home'">
+        <Home />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  import Navbar from './Navbar.vue'
+  import Home from './Home.vue'
+
+  export default {
+    name: 'Base',
+    components: {
+      Navbar,
+      Home
+    },
+    data () {
+      return {
+        currentRoute: this.$route.name
+      }
+    }
+  }
+</script>
